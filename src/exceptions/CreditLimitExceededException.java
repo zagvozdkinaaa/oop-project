ee7fac47d7923565143bc3c03396519e7d1df0ef
Zhanel <nursapazhan@gmail.com>
2026-05-01 17:13:20 +0500

    Update CreditLimitExceededException.java

--- src/exceptions/CreditLimitExceededException.java
+++ src/exceptions/CreditLimitExceededException.java
@@ -1,29 +1,31 @@
 package exceptions;
 
-/**
- * 
- */
-public class CreditLimitExceededException {
+public class CreditLimitExceededException extends Exception {
 
-    /**
-     * Default constructor
-     */
+    private static final int MAX_CREDITS = 21;
+
+    private final int attemptedCredits;
+
     public CreditLimitExceededException() {
+        super("Credit limit exceeded. Maximum allowed is " + MAX_CREDITS + " credits.");
+        this.attemptedCredits = MAX_CREDITS;
     }
 
-    /**
-     * @param credits
-     */
-    public CreditLimitExceededException(int credits) {
-        // TODO implement here
+    public CreditLimitExceededException(int attemptedCredits) {
+        super(buildMessage(attemptedCredits));
+        this.attemptedCredits = attemptedCredits;
     }
 
-    /**
-     * @return
-     */
-    public String getMessage() {
-        // TODO implement here
-        return "";
+    public int getAttemptedCredits() {
+        return attemptedCredits;
     }
 
+    public int getMaxCredits() {
+        return MAX_CREDITS;
+    }
+
+    private static String buildMessage(int attemptedCredits) {
+        return "Credit limit exceeded: attempted " + attemptedCredits +
+               " credits, but maximum allowed is " + MAX_CREDITS + ".";
+    }
 }
