a66f6ffeb13b1bf76458769b4c16a3b2b5fa51a0
Zhanel <nursapazhan@gmail.com>
2026-05-01 14:19:01 +0500

    Update Mark.java

--- src/academic/Mark.java
+++ src/academic/Mark.java
@@ -2,86 +2,69 @@ package academic;
 
 import users.Student;
 
-/**
- * 
- */
 public class Mark {
 
-    /**
-     * Default constructor
-     */
-    public Mark() {
-    }
-
-    /**
-     * 
-     */
     private String markId;
-
-    /**
-     * 
-     */
     private Student student;
-
-    /**
-     * 
-     */
     private Course course;
 
-    /**
-     * 
-     */
     private double firstAttestation;
-
-    /**
-     * 
-     */
     private double secondAttestation;
-
-    /**
-     * 
-     */
     private double finalExam;
 
-    /**
-     * 
-     */
     private double total;
 
+   
+    public Mark(String markId, Student student, Course course,
+                double firstAttestation, double secondAttestation, double finalExam) {
+        this.markId = markId;
+        this.student = student;
+        this.course = course;
+        this.firstAttestation = firstAttestation;
+        this.secondAttestation = secondAttestation;
+        this.finalExam = finalExam;
 
-
-
-
-    /**
-     * @return
-     */
-    public double getTotal() {
-        // TODO implement here
-        return 0.0d;
+        this.total = calculateTotal();
     }
 
-    /**
-     * @return
-     */
+   
+
     public double calculateTotal() {
-        // TODO implement here
-        return 0.0d;
+        this.total = firstAttestation + secondAttestation + finalExam;
+        return total;
     }
 
-    /**
-     * @return
-     */
+    public double getTotal() {
+        return total;
+    }
+
     public boolean isPassed() {
-        // TODO implement here
-        return false;
+        return total >= 50;
     }
 
-    /**
-     * @return
-     */
+    public String getLetterGrade() {
+    if (total > 100) total = 100;
+    if (total < 0) total = 0;
+
+    if (total >= 95) return "A";
+    else if (total >= 90) return "A-";
+    else if (total >= 85) return "B+";
+    else if (total >= 80) return "B";
+    else if (total >= 75) return "B-";
+    else if (total >= 70) return "C+";
+    else if (total >= 65) return "C";
+    else if (total >= 60) return "C-";
+    else if (total >= 55) return "D+";
+    else if (total >= 50) return "D";
+    else return "F";
+}
+
+  
+    @Override
     public String toString() {
-        // TODO implement here
-        return "";
+        return "Course: " + (course != null ? course.getName() : "N/A") +
+                ", Total: " + total +
+                ", Grade: " + getLetterGrade() +
+                ", Passed: " + (isPassed() ? "Yes" : "No");
     }
-
 }
