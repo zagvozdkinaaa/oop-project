db9aad05ca0413b792cc864dcb54fd156592a321
baiz170 <m_baizakov@kbtu.kz>
2026-05-17 22:20:23 +0500

    Update teacher name retrieval in toString method

--- src/academic/Lesson.java
+++ src/academic/Lesson.java
@@ -77,9 +77,10 @@ public class Lesson {
     public String toString() {
         return "Lesson: " + type +
                 ", course=" + (course != null ? course.getName() : "N/A") +
-                ", teacher=" + (teacher != null ? teacher.getName() : "N/A") +
+                ", teacher=" + (teacher != null ? teacher.getFullName() : "N/A") +
                 ", room=" + room +
                 ", date=" + date +
                 ", duration=" + duration + " min";
+
     }
 }
