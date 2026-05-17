a9c7fdb5045e38313ee4f568badb88aaa69753c0
baiz170 <m_baizakov@kbtu.kz>
2026-05-17 22:16:13 +0500

    Add files via upload

---
+++ src/users/ResearchAssociate.java
@@ -0,0 +1,88 @@
+package users;
+
+import core.Employee;
+import enums.UserRole;
+import research.ResearchPaper;
+import research.ResearchProject;
+import research.Researcher;
+import java.util.*;
+
+public class ResearchAssociate extends Employee implements Researcher {
+    private static final long serialVersionUID = 11L;
+
+    private String department;
+    private int hIndex;
+    private List<ResearchPaper> researchPapers;
+    private List<ResearchProject> researchProjects;
+    private boolean isResearcher = true;
+
+    public ResearchAssociate(String id, String firstName, String lastName, String email, String password,
+                            double salary, String department) {
+        super(id, firstName, lastName, email, password, UserRole.TEACHER, salary);
+        this.department = department;
+        this.hIndex = 0;
+        this.researchPapers = new ArrayList<>();
+        this.researchProjects = new ArrayList<>();
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public int getHIndex() {
+        return hIndex;
+    }
+
+    private void updateHIndex() {
+        this.hIndex = researchPapers.size();
+    }
+
+    @Override
+    public List<ResearchPaper> getResearchPapers() {
+        return researchPapers;
+    }
+
+    @Override
+    public void addResearchPaper(ResearchPaper paper) {
+        if (this.researchPapers == null) this.researchPapers = new ArrayList<>();
+        this.researchPapers.add(paper);
+        updateHIndex();
+    }
+
+    @Override
+    public List<ResearchProject> getResearchProjects() {
+        return researchProjects;
+    }
+
+    @Override
+    public void addResearchProject(ResearchProject project) {
+        if (this.researchProjects == null) this.researchProjects = new ArrayList<>();
+        this.researchProjects.add(project);
+    }
+
+    @Override
+    public void printPapers(Comparator<ResearchPaper> c) {
+        if (this.researchPapers != null) {
+            this.researchPapers.sort(c);
+            for (ResearchPaper p : this.researchPapers) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    @Override
+    public boolean isResearcher() {
+        return this.isResearcher;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | Department: %s | H-Index: %d | Papers: %d",
+                super.toString(), department, hIndex, researchPapers.size());
+    }
+}
---
+++ src/users/UserFactory.java
@@ -0,0 +1,52 @@
+package users;
+
+import core.User;
+import enums.UserRole;
+
+import java.util.*;
+
+public class UserFactory {
+
+    public UserFactory() {
+    }
+
+    public User createUser(String type, Map<String, Object> data) {
+        String id = (String) data.get("id");
+        String firstName = (String) data.get("firstName");
+        String lastName = (String) data.get("lastName");
+        String email = (String) data.get("email");
+        String password = (String) data.get("password");
+
+        double salary = (double) data.getOrDefault("salary", 0.0);
+
+        UserRole role;
+        try {
+            role = UserRole.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Invalid user type: " + type);
+            return null;
+        }
+
+        return switch (role) {
+            case ADMIN -> new Admin(id, firstName, lastName, email, password, salary);
+            case STUDENT -> {
+                String major = (String) data.getOrDefault("major", "Undeclared");
+                int year = (int) data.getOrDefault("year", 1);
+                yield new Student(id, firstName, lastName, email, password, major, year);
+            }
+            case TEACHER -> {
+                enums.TeacherPosition position = (enums.TeacherPosition) data.getOrDefault("position", enums.TeacherPosition.TUTOR);
+                yield new Teacher(id, firstName, lastName, email, password, salary,  position);
+            }
+            case MANAGER -> {
+                enums.ManagerType managerType = (enums.ManagerType) data.getOrDefault("managerType", enums.ManagerType.OR);
+                yield new Manager(id, firstName, lastName, email, password, managerType, salary);
+            }
+            default -> {
+                System.err.println("Unsupported user role: " + role);
+                yield null;
+            }
+        };
+    }
+
+}
